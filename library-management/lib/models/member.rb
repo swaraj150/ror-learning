dc7c11a64624319d94683a232409c75eb74ffe28
Swaraj <swaraj@incubyte.co>
2026-03-05 12:36:11 +0000

    add : add base Member class implementation

---
+++ library-management/lib/models/member.rb
@@ -0,0 +1,32 @@
+require 'date'
+
+class Member
+
+  attr_writer :name
+  attr_reader :id, :join_date, :borrowed_books
+
+  @@total_users = 0
+
+  def initialize(name)
+    @@total_users += 1
+    @id = @@total_users
+    @name = name
+    @join_date = Date.today
+    @borrowed_books = []
+  end
+
+  def borrow_book(book_id)
+    if @borrowed_books.include?(book_id)
+      puts "You've already borrowed this book."
+      return
+    end
+
+    @borrowed_books << book_id
+  end
+
+  def return_book(book_id)
+    @borrowed_books.delete(book_id)
+  end
+end
+  
+
