478e306794b5472ba7ad8cb6613db3afd0c985da
Swaraj <swaraj@incubyte.co>
2026-03-05 13:12:48 +0000

    add : add Library class which includes collection of books and member and respective methods

---
+++ library-management/lib/models/library.rb
@@ -0,0 +1,57 @@
+require 'book'
+require 'member'
+class Library
+  def initialize
+    @books   = {}
+    @members = {}
+  end
+
+  def add_book(title,author,quantity)
+    book = Book.new(title,author,quantity)
+    @books[book.id] = book
+  end
+
+  def register_member(name)
+    member = Member.new(name)
+    @members[member.id] = member
+  end
+
+  def get_member(member_id)
+    member = @members[member_id]
+    
+    if member.nil?
+      raise "Member with ID #{member_id} not found."
+    end
+    
+    return member
+  end
+  def get_book(book_id)
+    book = @books[book_id]
+    
+    if book.nil?
+      raise "Book with ID #{book_id} not found."
+    end
+
+    return book
+  end
+
+  def borrow_book(member_id, book_id)
+    member = get_member(member_id)
+    book   = get_book(book_id)
+
+    if book.available?
+      member.borrow_book(book_id)
+      book.check_out
+    else
+      puts "Sorry, '#{book.title}' is currently unavailable."
+    end
+  end
+
+  def return_book(member_id, book_id)
+    member = get_member(member_id)
+    book   = get_book(book_id)
+
+    member.return_book(book_id)
+    book.return_copy
+  end
+end
