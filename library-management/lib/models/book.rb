3b73d20803e84c93be06487427984bde815059a1
Swaraj <swaraj@incubyte.co>
2026-03-05 12:59:51 +0000

    refactor: refactor Book class by removing available_status attribute, removing checks in check_out and introducing return_copy method

--- library-management/lib/models/book.rb
+++ library-management/lib/models/book.rb
@@ -2,36 +2,33 @@ class Book
   attr_accessor :title, :author, :quantity
   attr_reader :id
 
-  @@total_count  = 0
+  @@total_books  = 0
 
-  def initialize(title, author, available_status, quantity)
-    @@total_count  += 1
-    @id = @@total_count
+  def initialize(title, author, quantity)
+    @@total_books  += 1
+    @id = @@total_books
     @title = title
     @author = author
-    @available_status = available_status
     @quantity = quantity
+    @available_status = quantity > 0
   end
 
   def available?
-    @available_status
+    quantity > 0
   end
 
   def check_out
-    unless @available_status 
-      puts "Sorry, '#{@title}' is not available."
-      return
-    end
-
     @quantity -= 1
-    @available_status = false if @quantity.zero?
-
     puts "Enjoy your #{title} book"
+  end 
+  
+  def return_copy
+    @quantity += 1
+    puts "Thank you for returning your #{title} book"
   end 
 
   def to_s
     "Book:\n  Title: #{@title}\n  Author: #{@author}\n  Available: #{available?}"
   end
 
-        
 end
