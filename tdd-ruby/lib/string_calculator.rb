b9128a42d552b0ca3a865be63d81a29884c02ce5
Swaraj <swaraj@incubyte.co>
2026-03-06 12:45:47 +0000

    GREEN : Implement StringCalculator#add to handle generalized cases of multiple number containing strings

--- tdd-ruby/lib/string_calculator.rb
+++ tdd-ruby/lib/string_calculator.rb
@@ -2,12 +2,7 @@ class StringCalculator
 
   def add(input)
     return 0 if input.empty?
-
-    if input == "5,6"
-      return 11
-    end
-
-    input.to_i
+    input.split(",").sum(&:to_i)
   end
 
 end
