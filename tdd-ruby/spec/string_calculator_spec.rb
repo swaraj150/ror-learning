96f90b02abb99d38275eba893be9ff0d339f65f3
Swaraj <swaraj@incubyte.co>
2026-03-06 12:56:34 +0000

    refactor : refactor spec file to use subject and context for DRY tests

--- tdd-ruby/spec/string_calculator_spec.rb
+++ tdd-ruby/spec/string_calculator_spec.rb
@@ -1,24 +1,21 @@
 require_relative '../lib/string_calculator'
 RSpec.describe StringCalculator do
   describe "#add" do
-    it "should return 0, when input is an empty string" do
-      calculator = StringCalculator.new
-      expect(calculator.add("")).to eq 0
+    subject(:calculator) { StringCalculator.new }
+
+    it "returns 0 when input is an empty string" do
+      expect(calculator.add("")).to eq(0)
     end
 
-    it "should return number itself, when input is a single number string" do
-      calculator = StringCalculator.new
-      expect(calculator.add("5")).to eq 5
-      expect(calculator.add("6")).to eq 6
-      expect(calculator.add("7")).to eq 7
+    context "when input is a single number" do
+      it { expect(calculator.add("5")).to eq(5) }
+      it { expect(calculator.add("6")).to eq(6) }
+      it { expect(calculator.add("7")).to eq(7) }
     end
 
-    it "should return sum of numbers, input contains multiple numbers" do
-      calculator = StringCalculator.new
-      expect(calculator.add("5,6")).to eq 11
-      expect(calculator.add("5,6,7")).to eq 18
+    context "when input contains multiple numbers" do
+      it { expect(calculator.add("5,6")).to eq(11) }
+      it { expect(calculator.add("5,6,7")).to eq(18) }
     end
-
-
   end
 end
