b77ff51bf37013a3de65957d1265289b5a84923f
Swaraj <swaraj@incubyte.co>
2026-03-06 13:23:18 +0000

    REFACTOR : extract delimiter extraction logic in StringCalculator#parse method

--- tdd-ruby/lib/string_calculator.rb
+++ tdd-ruby/lib/string_calculator.rb
@@ -1,13 +1,19 @@
 class StringCalculator
-  DELIMITER = /,|\n/
+  DEFAULT_DELIMITER = /,|\n/
 
   def add(input)
     return 0 if input.empty?
+    delimiter,numbers = parse(input)
+    numbers.split(delimiter).sum(&:to_i)
+  end
+
+  def parse(input)
     if input.start_with?("//")
       delimiter = input[2]
-      input.split(delimiter).sum(&:to_i)
+      numbers   = input.split("\n", 2).last
+      [delimiter, numbers]
     else
-      input.split(DELIMITER).sum(&:to_i)
+      [DEFAULT_DELIMITER, input]
     end
   end
 
