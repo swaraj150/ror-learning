96124662ef8a8c82842c4484de0d8703a3d38fd9
Swaraj <swaraj@incubyte.co>
2026-03-06 12:27:19 +0000

    GREEN : Implement StringCalculator#add to handle generalized cases of single number strings

--- tdd-ruby/lib/string_calculator.rb
+++ tdd-ruby/lib/string_calculator.rb
@@ -1,11 +1,16 @@
 class StringCalculator
 
   def add(input)
-    if input == "5"
-      return 5
-    elsif input == "6"
-      return 6
+    if input.length == 1
+      return input.to_i
     end
+
+
+    # if input == "5"
+    #   return 5
+    # elsif input == "6"
+    #   return 6
+    # end
     return 0
   end
 
