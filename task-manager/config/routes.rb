a9c1210ca7079334848c39032794f16b4c7b78fa
Swaraj <swaraj@incubyte.co>
2026-03-12 12:34:11 +0000

    add : add CRUD routes for User model

--- task-manager/config/routes.rb
+++ task-manager/config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "/users" => "users#index", as: :get_all_users
+  get "/users/:id" => "users#show", as: :get_user_by_id
+  post "/users" => "users#create", as: :create_user
+  patch "/users/:id" => "users#update", as: :update_user
+  delete "/users/:id" => "users#destroy", as: :destroy_user
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
