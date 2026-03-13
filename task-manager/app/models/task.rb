7b38f37a992f38dc40fe06282dc62df4f403043e
Swaraj <swaraj@incubyte.co>
2026-03-13 09:57:00 +0000

    add : add validations for task model

--- task-manager/app/models/task.rb
+++ task-manager/app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :user
+  validates :title, :status, presence: true
+  validates :priority,  numericality: { greater_than: 0, less_than_or_equal_to: 5 }
 end
