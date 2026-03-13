c46ee5f93aebcd4fe83a38635ff0a877bca8c4f9
Swaraj <swaraj@incubyte.co>
2026-03-13 09:56:42 +0000

    refactor : add nested resources for task

--- task-manager/config/routes.rb
+++ task-manager/config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get "/users" => "users#index", as: :get_all_users
-  get "/users/:id" => "users#show", as: :get_user_by_id
-  post "/users" => "users#create", as: :create_user
-  patch "/users/:id" => "users#update", as: :update_user
-  delete "/users/:id" => "users#destroy", as: :destroy_user
+  resources :users do
+    resources :tasks
+  end
+  
+
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
