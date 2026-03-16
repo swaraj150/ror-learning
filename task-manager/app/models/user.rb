6bc32220706b06dd0a3503e4511fd6a7742da173
Swaraj Andhale <swaraj@incubyte.co>
2026-03-16 13:04:30 +0530

    add : add has_secure_password to user model

--- task-manager/app/models/user.rb
+++ task-manager/app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
-  validates :name, :email, :password_digest, presence: true
+  has_secure_password
+  validates :name, :email, :password, presence: true
   validates :email, uniqueness: { case_sensitive: false }
-  validates :password_digest, length: { in: 8..20 }
+  validates :password, length: { in: 8..20 }
+
+  def as_json(options = {})
+    super(options.merge(only: [ :id, :name, :email ]))
+  end
 end
